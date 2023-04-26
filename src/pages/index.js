a3c99e71f24a2e01443f08c69cdfee2a1fb2851b
Abdul Rosid <rosid.dev@yahoo.com>
2023-04-26 16:52:57 +0700

    add finish responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
         <title>Fazzpay</title>
       </Head>
       <Navbar />
-      <div className="h-full w-screen bg-[#E5E5E5]">
-        <div className="w-4/5 grid grid-cols-2 gap-3 justify-center mx-auto">
-          <div className="flex">
-            <div className="justify-start">
+      <div className="h-full bg-[#E5E5E5] px-4 md:px-0">
+        <div className="max-w-7xl mx-auto py-12 md:py-10">
+          <div className="grid md:grid-cols-2 gap-12">
+            <div className="flex justify-center md:justify-start">
               <Image
                 src={"/images/phone.png"}
                 width={400}
@@ -21,23 +21,21 @@ export default function Home() {
                 alt="fazzpay mobile"
               />
             </div>
-          </div>
-          <div className="flex">
-            <div className="flex flex-col justify-center my-auto gap-10">
-              <h1 className="text-6xl font-bold text-[#3A3D42]">
+            <div className="flex flex-col justify-center mt-6 md:mt-0 md:ml-8">
+              <h1 className="text-4xl md:text-6xl font-bold text-[#3A3D42] leading-tight">
                 Awesome App For Saving{" "}
                 <span className="text-[#6379F4]">Time.</span>
               </h1>
-              <p className=" text-base">
+              <p className="mt-6 text-base md:text-lg">
                 We bring you a mobile app for banking problems that oftenly
                 wasting much of your times.
               </p>
-              <button className="h-12 w-32 bg-[#6379F4] rounded-md font-medium text-white">
+              <button className="mt-8 md:mt-12 h-12 w-40 md:w-48 bg-[#6379F4] rounded-md font-medium text-white">
                 Try It Free
               </button>
-              <div className="flex flex-col gap-3">
-                <h1>Available On :</h1>
-                <div className="flex gap-3 -ml-2">
+              <div className="flex flex-col gap-3 mt-8 md:mt-12">
+                <h1 className="text-base md:text-lg">Available On :</h1>
+                <div className="flex gap-3">
                   <Image
                     src={"/images/gplay.png"}
                     height={50}
@@ -56,56 +54,56 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-screen h-[300px] bg-[#473AD10F]">
-        <div className="w-4/5 h-full flex justify-around mx-auto">
-          <div className="my-auto">
+      <div className="w-screen min-h-screen lg:h-full bg-[#473AD10F] flex p-3 md:p-0">
+        <div className="w-4/5 min-h-[300px] flex flex-col md:flex-row md:justify-around mx-auto ">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/microsoft.png"}
               height={40}
               width={100}
               alt="microsoft"
             />
           </div>
-          <div className="my-auto">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/dropbox.png"}
               height={40}
               width={100}
               alt="drop box"
             />
           </div>
-          <div className="my-auto">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/hm.png"}
               height={40}
               width={100}
               alt="hm"
             />
           </div>
-          <div className="my-auto">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/airbnd.png"}
               height={40}
               width={100}
               alt="air bnd"
             />
           </div>
-          <div className="my-auto">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/canon.png"}
               height={40}
               width={100}
               alt="canon"
             />
           </div>
-          <div className="my-auto">
+          <div className="h-full mb-5 flex items-stretch">
             <Image
-              className="object-cover"
+              className="self-center m-auto object-cover"
               src={"/images/dell.png"}
               height={40}
               width={100}
@@ -114,16 +112,17 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-screen h-screen">
+      <div className="w-screen h-full lg:h-screen">
         <div className="w-4/5 h-full flex flex-col justify-evenly mx-auto">
           <h1 className="text-5xl font-bold text-center text-[#3A3D42]">
             <span className="text-[#6379F4]">About</span> the Application.
           </h1>
-          <p className="text-center  text-[#3A3D42]">
+          <p className="text-center text-[#3A3D42]">
             We have some great features from the application and it’s totally
-            free <br /> to use by all users around the world.
+            free
+            <br /> to use by all users around the world.
           </p>
-          <div className="grid grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             <div className="h-[300px] rounded-md flex flex-col justify-evenly">
               <div className="h-20 w-20 rounded-full bg-[#473AD11A] flex mx-auto">
                 <Image
@@ -181,10 +180,11 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-full w-screen bg-[#E5E5E5]">
-        <div className="w-4/5 grid grid-cols-2 gap-3 justify-center mx-auto">
-          <div className="flex">
-            <div className="justify-start">
+
+      <div className="h-full bg-[#E5E5E5]  md:mb-0">
+        <div className="w-full lg:w-4/5 mx-auto grid gap-6 lg:grid-cols-2 py-2">
+          <div className="flex justify-center lg:justify-start">
+            <div className="w-full lg:w-3/4 xl:w-1/2">
               <Image
                 src={"/images/phone.png"}
                 width={400}
@@ -193,36 +193,36 @@ export default function Home() {
               />
             </div>
           </div>
-          <div className="flex">
-            <div className="flex flex-col justify-center my-auto gap-10">
-              <h1 className="text-6xl font-bold text-[#3A3D42]">
+          <div className="flex justify-center lg:justify-end">
+            <div className="w-full lg:w-3/4 flex flex-col justify-center gap-8">
+              <h1 className="text-3xl lg:text-6xl font-bold text-[#3A3D42] leading-tight">
                 All The <span className="text-[#6379F4]">Great.</span> Fazzpay
                 Features.
               </h1>
-              <div className="h-20 bg-white rounded-lg p-3">
-                <h1 className="font-medium text-xl text-[#3A3D42]">
+              <div className="min-h-20 bg-white rounded-lg p-4">
+                <h2 className="font-medium text-lg lg:text-xl text-[#3A3D42]">
                   <span className="text-[#6379F4]">1. </span>Small Fee
-                </h1>
+                </h2>
                 <p className="text-[#3A3D42] ml-1">
                   We only charge 5% of every success transaction done in FazzPay
                   app.
                 </p>
               </div>
-              <div className="h-20 bg-white rounded-lg p-3">
-                <h1 className="font-medium text-xl text-[#3A3D42]">
+              <div className="min-h-20 bg-white rounded-lg p-3">
+                <h2 className="font-medium text-lg lg:text-xl text-[#3A3D42]">
                   <span className="text-[#6379F4]">2. </span>Data Secured
-                </h1>
+                </h2>
                 <p className="text-[#3A3D42] ml-1">
                   All your data is secured properly in our system and it’s
                   encrypted.
                 </p>
               </div>
-              <div className="h-20 bg-white rounded-lg p-3">
-                <h1 className="font-medium text-xl text-[#3A3D42]">
+              <div className="min-h-20 bg-white rounded-lg p-3">
+                <h2 className="font-medium text-lg lg:text-xl text-[#3A3D42]">
                   <span className="text-[#6379F4]">3. </span>User Friendly
-                </h1>
+                </h2>
                 <p className="text-[#3A3D42] ml-1">
-                  FazzPay come up with modern and sleek design and not
+                  FazzPay comes with a modern and sleek design and is not
                   complicated.
                 </p>
               </div>
@@ -230,19 +230,19 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-screen h-screen relative sm:pb-10">
-        <div className="w-4/5 h-full flex flex-col justify-evenly mx-auto">
+
+      <div className="relative min-h-screen pb-14 md:pb-10">
+        <div className="w-4/5 mx-auto min-h-screen flex flex-col justify-evenly">
           <h1 className="text-5xl font-bold text-center text-[#3A3D42]">
             {" "}
-            What Users are
-            <span className="text-[#6379F4]"> Saying.</span>
+            What Users are<span className="text-[#6379F4]"> Saying.</span>
           </h1>
-          <p className="text-center  text-[#3A3D42]">
+          <p className="text-center text-[#3A3D42]">
             We have some great features from the application and it’s totally
-            free <br /> to use by all users around the world.
+            free to use by all users around the world.
           </p>
-          <div className="flex gap-5 justify-center mx-auto w-full items-stretch">
-            <div className="absolute sm:bottom-0 sm:left-[40%] lg:self-center">
+          <div className="flex flex-col sm:flex-row gap-5 justify-center mx-auto items-stretch">
+            <div className="absolute bottom-1 left-[30%] lg:static lg:self-center">
               <button class="bg-white shadow-lg hover:bg-primary text-white font-bold py-2 px-4 rounded-lg flex items-center">
                 <svg
                   class="h-5 w-5 text-black transform -rotate-90"
@@ -269,20 +269,20 @@ export default function Home() {
                   height={80}
                   alt="customer fazzpay"
                 />
-                <h1 className="text-center font-semibold  text-xl text-[#373C46]">
+                <h1 className="text-center font-semibold text-xl text-[#373C46]">
                   Alex Hansinburg
                 </h1>
                 <h3 className="text-center text-[#56585B]">Designer</h3>
-                <p className="content-center text-center px-16">
-                  “This is the most outstanding app that I’ve ever try in my
-                  live, this app is such an amazing masterpiece and it’s
-                  suitable for you who is bussy with their bussiness and must
-                  transfer money to another person aut there. Just try this app
+                <p className="text-center px-16">
+                  “This is the most outstanding app that I’ve ever tried in my
+                  life, this app is such an amazing masterpiece and it’s
+                  suitable for you who is busy with their business and must
+                  transfer money to another person out there. Just try this app
                   and see the power!”
                 </p>
               </div>
             </div>
-            <div className="absolute sm:bottom-0 sm:right-[40%] lg:self-center">
+            <div className="absolute bottom-1 right-[30%] lg:static lg:self-center">
               <button class="bg-white shadow-lg hover:bg-primary text-white font-bold py-2 px-4 rounded-lg flex items-center">
                 <svg
                   class="h-5 w-5 text-black transform rotate-90"
@@ -303,6 +303,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+
       <Landingpage />
     </>
   );
