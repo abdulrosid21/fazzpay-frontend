98a194be25b1e76d0c6dead291cc6eab59bd6196
Abdul Rosid <rosid.dev@yahoo.com>
2023-04-26 19:47:13 +0700

    add readme.md to repository

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,29 +9,9 @@ function Navbar() {
   const [hamburger, setHamburger] = useState(false);
   const token = Cookies.get("token");
   return (
-    // <div className="w-screen h-24 bg-[#E5E5E5] rounded-b-md shadow-md">
-    //   <div className="w-[80%] h-full flex justify-between mx-auto">
-    //     <h1 className="my-auto text-5xl font-bold text-[#6379F4] cursor-pointer">
-    //       FazzPay
-    //     </h1>
-    //     <div className="my-auto flex gap-5">
-    //       <Link href="/signin">
-    //         <button className="w-24 h-10 bg-transparent rounded-md border-2 border-[#6379F4] text-[#6379F4] font-nunito">
-    //           Login
-    //         </button>
-    //       </Link>
-
-    //       <Link href="/signup">
-    //         <button className="w-24 h-10 bg-[#6379F4] border-2 border-[#6379F4] rounded-md text-white font-nunito">
-    //           Sign Up
-    //         </button>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
     <>
       <Head>
-        <title>FazzPay</title>
+        <title>Fazzpay</title>
       </Head>
       <header className={`${hamburger ? "h-screen" : ""} flex flex-col`}>
         <div className="flex justify-between items-center shadow-md p-4 sm:p-6 xl:px-12">
